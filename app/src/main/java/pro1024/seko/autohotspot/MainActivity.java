fe6d91f0c4a27c9bfb879228169e0ead73cb20e1
sebakow09 <skowalik09@gmail.com>
2017-01-12 21:57:31 +0100

    12.01.2017 - Poprawki pkt.1
    Punkt 1: Obecnie sytuacja wygląda następująco. Jeżeli w ciągu 5 minut
    zużycie danych będzie poniżej 500KB(czyli dość małe, ale poinformuj
    mnie, jeżeli byłoby to za dużo i lepsza by była jakaś mniejsza wartość),
    to hotspot zostanie wyłączony razem z aplikacją.
    Więc jeżeli się wyłączy, a ty będziesz chciał znowu włączyć hotspot, to
    po prostu uruchom aplikację (jeżeli oczywiście ładowarka jest włączona
    to hotspot się uruchomi).
    - Dodałem licznik pokazujący użycie danych.
    - Plus jakieś drobne poprawki w kodzie i wyglądzie aplikacji.﻿

--- app/src/main/java/pro1024/seko/autohotspot/MainActivity.java
+++ app/src/main/java/pro1024/seko/autohotspot/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.TrafficStats;
 import android.net.wifi.WifiManager;
 import android.os.BatteryManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     static WifiAP wifiAp;
     private WifiManager wifi;
     private MyReceiver myReceiver;
+    final double [] OTOld = new double [1];
+    double currentData = 0;
+    double currentData_2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +32,70 @@ public class MainActivity extends AppCompatActivity {
         initReceiver();
 
         //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-    }
 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView infoView = (TextView) findViewById(R.id.traffic_info);
+                                String info = "";
+
+                                info += "Dane komórkowe:\n";
+                                info += ("\tOdebrane: " + TrafficStats.getMobileRxBytes() / 1024 + " KB(" + TrafficStats.getMobileRxBytes() / 1048576 + " MB)");
+                                info += ("\tWysłane: " + TrafficStats.getMobileTxBytes() / 1024 + " KB(" + TrafficStats.getMobileTxBytes() / 1048576 + " MB)");
+
+                                info += "\nWszystkie interfejsy sieciowe:\n";
+                                info += ("\tOdebrane: " + TrafficStats.getTotalRxBytes() / 1024 + " KB(" + TrafficStats.getTotalRxBytes() / 1048576 + " MB)");
+                                info += ("\tWysłane: " + TrafficStats.getTotalTxBytes() / 1024 + " KB(" + TrafficStats.getTotalTxBytes() / 1048576 + " MB)");
+
+                                infoView.setText(info);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread.start();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                double overallTraffic = TrafficStats.getTotalRxBytes()/1024;
+                currentData = currentData + overallTraffic - OTOld [0];
+                currentData_2 += currentData;
+
+                if(currentData_2<500){
+                    /*TextView view2 = (TextView) findViewById(R.id.yn);
+                    view2.setText("Czy przekroczyło 500KB? - NIE " + currentData + " i " + currentData_2);*/
+                    finishReceiver();
+                    wifiAp.toggleWiFiAP(wifi, MainActivity.this);
+                    killApp();
+                }
+                else{
+                    /*TextView view2 = (TextView) findViewById(R.id.yn);
+                    view2.setText("Czy przekroczyło 500KB? - TAK " + currentData + " i " + currentData_2);*/
+                    currentData = 0;
+                    currentData_2 = 0;
+                }
+
+                /*TextView view1 = (TextView) findViewById(R.id.test);
+                view1.setText("Current Data = " + currentData);*/
+
+                OTOld [0] = overallTraffic;
+                currentData = 0;
+
+                handler.postDelayed(this, 300000);
+            }
+        }, 300000 );
+
+    }
     private void initReceiver(){
         myReceiver = new MyReceiver();
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
@@ -87,5 +154,11 @@ public class MainActivity extends AppCompatActivity {
             //btnWifiToggle.setText("Turn on");
             //findViewById(R.id.bg).setBackgroundResource(R.drawable.bg_wifi_off);
         }
+    }
+
+    public void killApp(){
+        MainActivity.super.onDestroy();
+        finish();
+        System.exit(0);
     }
 }
